e338d56f0fe8e625c0f9de16eeeaa9d9f0fab26c
moons2 <moraes.luan1997@gmail.com>
2022-08-21 16:59:47 -0300

    list prof home

--- src/main/java/devweb/controller/HomeController.java
+++ src/main/java/devweb/controller/HomeController.java
@@ -1,19 +1,24 @@
 package devweb.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import devweb.service.spec.IProfissionalService;
 
 @Controller
+@RequestMapping("/")
 public class HomeController {
+	
+	@Autowired
 	private IProfissionalService service;
     
 	// ESSA PÁGINA INICIAL POR ENQUANTO É A PÁGINA DO ADMIN, PRECISA CRIAR A PÁGINA INICIAL AINDA
     @GetMapping("/")
 	public String home(ModelMap model) {
-    	model.addAttribute("clientes",service.buscarTodos());
+    	model.addAttribute("profissionais", service.buscarTodos());
 		return "home";
 	}
 }
