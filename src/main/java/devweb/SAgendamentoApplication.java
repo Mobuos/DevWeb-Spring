bbb76651b0e33a0582fb7fa28ba1df33c90949bb
wandersonmn <wanderson.mn@hotmail.com.br>
2022-08-20 21:25:20 -0300

    arquivos profissionais

--- src/main/java/devweb/SAgendamentoApplication.java
+++ src/main/java/devweb/SAgendamentoApplication.java
@@ -34,7 +34,7 @@ public class SAgendamentoApplication {
 			c1.setSenha("6422");
 			c1.setSexo('M');
 			clienteDAO.save(c1);
-<<<<<<< HEAD
+
 			
 			//Profissional p1 = new Profissional("12345678910","pro1@profissional.com", "pro", "profissional 1", 'm', "11987654321", "20/08/20222","areap1","especialidadep1","qualip1");
 			//profissionalDAO.save(p1);
@@ -81,60 +81,4 @@ public class SAgendamentoApplication {
 		
 		
 	}
-	/*
-	@Bean
-	public CommandLineRunner demo(IProfissionalDAO profissionalDAO) {
-		return (args) -> {
-
-
-			
-			Profissional p1 = new Profissional("12345678910","pro1@profissional.com", "pro", "profissional 1", 'm', "11987654321", "20/08/20222","areap1","especialidadep1","qualip1");
-			profissionalDAO.save(p1);
-			
-						
-			// Editora e1 = new Editora();
-			// e1.setCNPJ("55.789.390/0008-99");
-			// e1.setNome("Companhia das Letras");
-			// editoraDAO.save(e1);
-			
-			// Editora e2 = new Editora();
-			// e2.setCNPJ("71.150.470/0001-40");
-			// e2.setNome("Record");
-			// editoraDAO.save(e2);
-			
-			// Editora e3 = new Editora();
-			// e3.setCNPJ("32.106.536/0001-82");
-			// e3.setNome("Objetiva");
-			// editoraDAO.save(e3);
-			
-			// Livro l1 = new Livro();
-			// l1.setTitulo("Ensaio sobre a Cegueira");
-			// l1.setAutor("José Saramago");
-			// l1.setAno(1995);
-			// l1.setPreco(BigDecimal.valueOf(54.9));
-			// l1.setEditora(e1);
-			// livroDAO.save(l1);
-			
-			// Livro l2 = new Livro();
-			// l2.setTitulo("Cem anos de Solidão");
-			// l2.setAutor("Gabriel Garcia Márquez");
-			// l2.setAno(1977);
-			// l2.setPreco(BigDecimal.valueOf(59.9));
-			// l2.setEditora(e2);
-			// livroDAO.save(l2);
-			
-			// Livro l3 = new Livro();
-			// l3.setTitulo("Diálogos Impossíveis");
-			// l3.setAutor("Luis Fernando Verissimo");
-			// l3.setAno(2012);
-			// l3.setPreco(BigDecimal.valueOf(22.9));
-			// l3.setEditora(e3);
-			// livroDAO.save(l3);
-=======
->>>>>>> a875c05ecdfa162444238ed9afd6d201b74011c2
-		};
-		
-		
-	}
-	*/
 }
