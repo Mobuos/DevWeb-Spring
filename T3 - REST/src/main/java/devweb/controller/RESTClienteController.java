a708eb8aec115852f991049a4e6fda6f4d5cb9c7
João Dini <copperbrjdmdini@gmail.com>
2022-09-05 17:18:08 -0300

    Altera cliente
    `CRUD Cliente`

--- T3 - REST/src/main/java/devweb/controller/RESTClienteController.java
+++ T3 - REST/src/main/java/devweb/controller/RESTClienteController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -97,12 +98,23 @@ public class RESTClienteController {
     }
 
     // Atualiza o cliente de id = {id}
-    // @GetMapping(path = "/clientes")
-    // public ResponseEntity<List<Cliente>> lista() {
-    //     List<Cliente> lista = cService.buscarTodos();
-    //     if (lista.isEmpty()) {
-    //         return ResponseEntity.notFound().build();
-    //     }
-    //     return ResponseEntity.ok(lista);
-    // }
+    @PutMapping(path = "/api/clientes/{id}")
+	public ResponseEntity<Cliente> atualiza(@PathVariable("id") long id, @RequestBody JSONObject json) {
+		try {
+			if (isJSONValid(json.toString())) {
+				Cliente cliente = cService.buscarPorId(id);
+				if (cliente == null) {
+					return ResponseEntity.notFound().build();
+				} else {
+					parse(cliente, json);
+					cService.salvar(cliente);
+					return ResponseEntity.ok(cliente);
+				}
+			} else {
+				return ResponseEntity.badRequest().body(null);
+			}
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(null);
+		}
+ }
 }
